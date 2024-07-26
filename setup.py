db9bea215752dfd77f225c2d82ea452729e8d1d3
Sarita Nandal <sarita.n@breachlock.com>
2024-07-26 16:31:02 +0530

    added first version of project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sarita Nandal",
+    author_email="sarita@ma.iitr.ac.in",
+    packages=find_packages(),
+    install_requires=[],
+)
