a86a84b9bfe3da2e9a8bfeeb5a7f97efbe714132
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2025-01-05 15:23:54 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from pymongo import MongoClient
 from zipfile import Path
-from src.constant import *
+from src.constant import * 
 from src.exception import CustomException
 from src.logger import logging
 
